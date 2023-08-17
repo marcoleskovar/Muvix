b0317a86b62203a2e167b5ee060016ac87c7580f
Marco Leskovar <leskovarmarco@gmail.com>
2023-08-17 18:44:15 -0300

    navbar V1.2 (mejoras con scroll)

--- js/header.js
+++ js/header.js
@@ -1,10 +1,17 @@
-const header = document.getElementById('mainHeader')
+const header = document.getElementById('mainHeader');
+let currentScrollY = 0;
+let lastScrollY = 0;
 
-const hideHeaderScrollDown = () => {
-    let lastScrollY = window.scrollY
-    window.onscroll = ()=>{
-        lastScrollY < window.scrollY ? header.classList.add('nav-hidden') : header.classList.remove('nav-hidden')
-        lastScrollY = window.scrollY
+const handleScroll = () => {
+    currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY + 10) {
+        header.classList.add('nav-hidden');
+    } else if (currentScrollY < lastScrollY - 10) {
+        header.classList.remove('nav-hidden');
     }
-}
-hideHeaderScrollDown()
+
+    lastScrollY = currentScrollY;
+};
+
+window.addEventListener('scroll', handleScroll);
