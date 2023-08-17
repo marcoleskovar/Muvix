dba797daf5a2293cd80aa0e43aeb4563155bec25
Marco Leskovar <leskovarmarco@gmail.com>
2023-08-17 16:44:57 -0300

    navbar V1.1 (se esconde con scroll-down)

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ const header = document.getElementById('mainHeader')
 const hideHeaderScrollDown = () => {
     let lastScrollY = window.scrollY
     window.onscroll = ()=>{
-        lastScrollY < window.scrollY ? header.classList.add('hidden') : header.classList.remove('hidden')
+        lastScrollY < window.scrollY ? header.classList.add('nav-hidden') : header.classList.remove('nav-hidden')
         lastScrollY = window.scrollY
     }
 }
