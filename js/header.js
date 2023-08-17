55d4e11918ea99b443af3aae069ea1ec6092ab03
Marco Leskovar <leskovarmarco@gmail.com>
2023-08-17 19:11:20 -0300

    navbar V1.3 (reemplaza if por ternario)

--- js/header.js
+++ js/header.js
@@ -4,13 +4,8 @@ let ultimoScroll = 0;
 
 const scrollHideNav = () => {
     actualScroll = window.scrollY;
-
-    if (actualScroll > ultimoScroll + 10) {
-        header.classList.add('nav-hidden');
-    } else if (actualScroll < ultimoScroll - 5) {
-        header.classList.remove('nav-hidden');
-    }
-    ultimoScroll = actualScroll;
+    actualScroll > ultimoScroll + 10 ? header.classList.add('nav-hidden') : (actualScroll < ultimoScroll - 5 ? header.classList.remove('nav-hidden') : false)
+    ultimoScroll = actualScroll; 
 };
 
 window.onscroll = scrollHideNav;
